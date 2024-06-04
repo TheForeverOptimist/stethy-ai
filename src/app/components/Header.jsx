08567e0355d526d494ef11f84303cc50bb1b8fe8
TheForeverOptimist <sean@elevate.app>
2024-06-03 22:46:42 -0700

    added theme providers, global colors, nav bar

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header(){
+    return (<nav>
+        <h1>Stethy AI</h1>
+    </nav>
+)}
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '@/app/components/Header'
+
+export default function Home(){
+    return(
+        <main>
+            <Header />
+            <h1>Welcome to Stethy AI</h1>
+        </main>
+    )
+}
