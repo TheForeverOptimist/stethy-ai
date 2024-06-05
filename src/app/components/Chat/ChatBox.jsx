e7094dd3aa4f1c6b0b7be82e72bc6699da5fb4d6
TheForeverOptimist <sean@elevate.app>
2024-06-04 21:08:55 -0700

    added new global ui css

--- src/app/components/Chat/ChatBox.jsx
+++ src/app/components/Chat/ChatBox.jsx
@@ -25,7 +25,12 @@ export default function ChatBox(){
                 ))}
             </div>
             <div className='input-area'>
-                
+                <input 
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button onClick={sendMessage}>Send</button>
             </div>
         </div>
     )
